4d9cd848308ce68fa38f9884aa2f0e9d06f55213
Mateus Fernandes Pereira <mateus161004@gmail.com>
2025-11-17 20:47:49 -0300

    feature: score entry

--- src/entities/ScoreEntry.java
+++ src/entities/ScoreEntry.java
@@ -1,15 +1,13 @@
+
 package entities;
 
+/**
+ * Representa uma entrada no ranking de pontuacoes.
+ */
 public class ScoreEntry {
     public String name;
     public int score;
 
-    /**
-     * Construtor da entrada de pontuação.
-     * 
-     * @param name nome do jogador
-     * @param score pontuação
-     */
     public ScoreEntry(String name, int score) {
         this.name = name;
         this.score = score;
